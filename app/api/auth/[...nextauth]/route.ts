8c56f9a9eff4cb7caf059accc48f16024e97af0b
ankitsensei <ankitbhagat.dev@gmail.com>
2026-09-09 22:35:33 +0530

    feat: implement MongoDB connection and user authentication logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import dbConnect from "@/lib/dbConnect";
+import { User } from "lucide-react";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -15,6 +17,50 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      console.log("This is token: ", token);
+      console.log("This is user: ", user);
+      console.log("This is account: ", account);
+
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+    async signIn({ user, profile }) {
+      await dbConnect();
+      let dbUser = await User.findOne({ email: user.email });
+
+      // if user not found create new user
+      if (!dbUser) {
+        dbUser = await User.create({
+          name: profile.name,
+          email: profile.email,
+          profilePicture: profile.profilePicture,
+          isVerified: profile.email_verified ? true : false,
+        });
+      }
+      user.id = dbUser._id.toString();
+      return true;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 90 * 24 * 60 * 60,
+  },
+  pages: {
+    singIn: "user-auth",
+  },
 };
 
-export default NextAuth(authOptions);
+const handle = NextAuth(authOptions);
+export { handle as POST, handle as GET };
