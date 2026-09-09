9281acbfb507c58c2124ed24b1cde66a3d1bbdff
ankitsensei <ankitbhagat.dev@gmail.com>
2026-09-09 22:23:18 +0530

    feat: created userSchema

---
+++ models/User.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String },
+    profilePicture: { type: String },
+    isVerified: { type: Boolean, required: true },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
