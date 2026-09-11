505f79f24611d63389b7d10494657824e5c9941c
ankitsensei <ankitbhagat.dev@gmail.com>
2026-09-11 23:20:25 +0530

    feat: restructure page layout and add Header, MeetingAction, and MeetingFeature components

---
+++ components/ui/MeetingAction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MeetingAction = () => {
+  return (
+    <div>MeetingAction</div>
+  )
+}
+
+export default MeetingAction
---
+++ components/ui/MeetingFeature.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MeetingFeature = () => {
+  return (
+    <div>MeetingFeature</div>
+  )
+}
+
+export default MeetingFeature
